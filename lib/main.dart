b6b6c90490b3aeb4ebb887ad8a5e7ad665e3cf6f
Tanvir Rahman Rafi <tanvirrahmanrafi007@gmail.com>
2023-09-30 23:56:48 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,383 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final isWideScreen = MediaQuery.of(context).size.width > 500;
+
+    return Scaffold(
+      backgroundColor: (Colors.grey.shade200),
+      appBar: AppBar(
+        title: const Text('Profile'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Center(
+        child: isWideScreen
+            ? const LandScapeScreenProfile() // Landscape layout
+            : const PortraitScreenProfile(), // Portrait layout
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LandScapeScreenProfile extends StatelessWidget {
+  const LandScapeScreenProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Row(
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: CircleAvatar(
+            radius: 180,
+            backgroundImage: NetworkImage(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Arafat Rahman',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 100, // Fixed height to enable scrolling
+                  child: ListView(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ResponsiveGridRow(
+                  children: [
+                    ResponsiveGridCol(
+                      xs: 12,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=10',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=11',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=12',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                ResponsiveGridRow(
+                  children: [
+                    ResponsiveGridCol(
+                      xs: 12,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=13',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=14',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=15',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                ResponsiveGridRow(
+                  children: [
+                    ResponsiveGridCol(
+                      xs: 12,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=16',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=17',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.network(
+                                'https://picsum.photos/250?image=18',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+class PortraitScreenProfile extends StatelessWidget {
+  const PortraitScreenProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: CircleAvatar(
+        radius: 180,
+        backgroundImage: NetworkImage(
+          'https://picsum.photos/250?image=9',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+    const Text(
+    'Arafat Rahman',
+    style: TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+    ),
+    ),
+    const SizedBox(height: 😎,
+    Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: SizedBox(
+    height: 280,
+    child: ListView(
+    children: [
+    const Text(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    textAlign: TextAlign.left,
+    style: TextStyle(fontSize: 16),
+    ),
+    ResponsiveGridRow(
+    children: [
+    ResponsiveGridCol(
+    xs: 12,
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=10',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=11',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=12',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    ],
+    ),
+    ),
+    ],
+    ),
+    ResponsiveGridRow(
+    children: [
+    ResponsiveGridCol(
+    xs: 12,
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=13',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=14',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=15',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    ],
+    ),
+    ),
+    ],
+    ),
+    ResponsiveGridRow(
+    children: [
+    ResponsiveGridCol(
+    xs: 12,
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=16',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=17',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+
+    Expanded(
+    flex: 4,
+    child: Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Image.network(
+    'https://picsum.photos/250?image=18',
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    ],
+    ),
+    ),
+    ],
+    ),
+    ],
+    ),
+    ),
+    ),
+    ],
+    ),
     );
   }
 }
